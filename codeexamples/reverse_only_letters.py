b2f19b1b8136b0affdcb7c556c0c6e891f7407c4
vkvn <63067113+vkvn@users.noreply.github.com>
2021-05-12 11:11:48 +0200

    Create reverse_only_letters.py

---
+++ codeexamples/reverse_only_letters.py
@@ -0,0 +1,16 @@
+def reverse_only_letters(text):
+    """ leetcode 917"""
+    s,i,j = list(text), 0, len(text)-1
+    while i<j:
+        if ( s[i].isalpha()) and (s[j].isalpha() ):
+            s[i],s[j]= s[j],s[i]
+            i +=1
+            j -=1
+        elif not (s[i].isalpha()):
+            i +=1
+        elif not (s[j].isalpha()):
+            j -=1
+    #print (type (str(s)))
+    return print(str(s))
+
+reverse_only_letters("H3ll0 W0r$")
