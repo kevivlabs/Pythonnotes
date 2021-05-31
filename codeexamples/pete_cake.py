dc5772fd4f42b1219832b3b1bf0dad2d5f9ea4ec
vkvn <63067113+vkvn@users.noreply.github.com>
2021-05-31 10:43:57 +0200

    Stop writing ugly code (x_x)
    Atleast it passed all the test cases but still super ugly.

--- codeexamples/pete_cake.py
+++ codeexamples/pete_cake.py
@@ -1,16 +1,32 @@
 def cakes(recipe, available):
     # TODO: insert code
     t=[]
-    for i in recipe:
-        if i in available:
-            t.append(available[i] // recipe[i])
-            return (min(t))
-        else:
-            return 0
-          
-'''
-recipe ={'cream': 200, 'flour': 300, 'sugar': 150, 'milk': 100, 'oil': 100}
-available = {'sugar': 1700, 'flour': 20000, 'milk': 20000, 'oil': 30000, 'cream': 5000}
-cakes(recipe, available)
-Fails : Need to check the code
-'''
+    if len(recipe)<=len(available):
+        for i in recipe:
+            for j in available:
+                if i not in available: 
+                    return 0
+                else:
+                    if i==j:
+                        
+                        t.append(available[j] // recipe[i])
+    else:
+        return 0
+    
+    
+    return (min(t))
+                
+                
+            
+     
+'''        
+recipe= {'cream': 200, 'flour': 300, 'sugar': 150, 'milk': 100, 'oil': 100}
+availabe= {'sugar': 1700, 'flour': 20000, 'milk': 20000, 'oil': 30000, 'cream': 5000}       
+#recipe = {'flour': 78, 'butter': 76, 'sugar': 35}
+#available = {'crumbles': 8774, 'cocoa': 3484, 'oil': 664, 'eggs': 9322, 'butter': 6339, 'nuts': 8775, 'pears': 7239, 'apples': 1389, 'sugar': 1643, 'milk': 7924}
+cakes(recipe,available)
+
+better solution 
+def cakes(recipe, available):
+    return min(available.get(k, 0)/recipe[k] for k in recipe)
+    '''
